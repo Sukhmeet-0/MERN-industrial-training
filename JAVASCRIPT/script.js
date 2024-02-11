fbefda878bf32b45a0aba122a114371156d2721a
Sukhmeet-0 <sukhmeets111@gmail.com>
2024-02-11 22:57:00 +0530

    adding more js files

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+var printValues =()=>{
+    var name = (document.getElementById('name').value)
+    // document.write('<h1>'+name+'</h1>'
+    // )
+    document.getElementById('output').innerHTML=name
+}
