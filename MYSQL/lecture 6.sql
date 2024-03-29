21595c66084317971dd8a5ee0171a76b6e16b011
Sukhmeet-0 <sukhmeets111@gmail.com>
2024-03-29 23:31:02 +0530

    adding lecture 6

---
+++ MYSQL/lecture 6.sql
@@ -0,0 +1,8 @@
+show databases;
+use training;
+select * from students;
+select fullname from students;
+select * from students where mobile='9988899879';
+select count(*) from students;
+select count(*) as total from students;
+-- select min,max,avg(salary) as minmob from studens;
