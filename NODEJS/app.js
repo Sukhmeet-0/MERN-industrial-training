1fe4d290794ff2abc8ec90c11bbee84fab36b6ab
Sukhmeet-0 <sukhmeets111@gmail.com>
2024-04-14 15:52:35 +0530

    node lec2

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-    res.write('<h1>Hello ki haaaaaaal aaaaaaa g</h1>')
+    // res.write('<h1>Hello ki haaaaaaal aaaaaaa g</h1>')
+    res.setHeader('Content-Type','text/html')
+    res.write('<h1>Hello </h1>')
+    res.write('<p>lorem100 </p>')
+    res.end()
 } )
 
 app.get('/about',(req,res)=>{
-    res.send('<h2>About us</h2>')
+    // res.send('<h2>About us</h2>')
+    res.sendFile(path.join(__dirname)+'/Pages/HomePage.html')
 })
 
 app.listen(1699,()=>{
