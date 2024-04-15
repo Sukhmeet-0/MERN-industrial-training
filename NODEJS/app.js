7f0ee0300f68a67277ba7f643176c54e82b14cc6
Sukhmeet-0 <sukhmeets111@gmail.com>
2024-04-15 22:11:06 +0530

    node lec 3

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     // res.write('<h1>Hello ki haaaaaaal aaaaaaa g</h1>')
-    res.setHeader('Content-Type','text/html')
-    res.write('<h1>Hello </h1>')
-    res.write('<p>lorem100 </p>')
-    res.end()
+    res.sendFile(path.join(__dirname)+'/Pages/HomePage.html')
+    // res.setHeader('Content-Type','text/html')
+    // res.write('<h1>Hello </h1>')
+    // res.write('<p>lorem100 </p>')
+    // res.end()
 } )
 
 app.get('/about',(req,res)=>{
@@ -16,6 +18,13 @@ app.get('/about',(req,res)=>{
     res.sendFile(path.join(__dirname)+'/Pages/HomePage.html')
 })
 
+app.get('/signin',(req,res)=>{
+    console.log(req.query)
+})
+
+app.post('/signin',(req,res)=>{
+    console.log(req.body)
+})
 app.listen(1699,()=>{
     console.log('Server started on port 1699')
 })
