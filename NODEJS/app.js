99c2d9fd0856069b60b364a170e1bbd12c8dc0c2
Sukhmeet-0 <xsukhmeet@gmail.com>
2024-04-17 11:23:43 +0530

    adding node lec 4

--- NODEJS/app.js
+++ NODEJS/app.js
@@ -4,6 +4,21 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'demo'
+})
+connection.connect((error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log('Db connected')
+    }
+})
+
 app.get('/',(req,res)=>{
     // res.write('<h1>Hello ki haaaaaaal aaaaaaa g</h1>')
     res.sendFile(path.join(__dirname)+'/Pages/HomePage.html')
@@ -25,6 +40,21 @@ app.get('/signin',(req,res)=>{
 app.post('/signin',(req,res)=>{
     console.log(req.body)
 })
+let studId=1002
+let fullname='harmeet singh'
+let mobile='9815161041'
+app.get('/insert',(req,res)=>{
+    // const insertQuery = 'Insert into students(studId,fullname,mobile) values(1001,"sukhmeet singh","9988899879")';
+    const insertQuery = `Insert into students(studId,fullname,mobile) values(${studId},'${fullname}','${mobile}')`
+    connection.query(insertQuery,(error)=>{
+        if(error){
+            console.log(error)
+        }else{
+            console.log('Inserted value into table')
+        }
+    })
+})
+
 app.listen(1699,()=>{
     console.log('Server started on port 1699')
 })
