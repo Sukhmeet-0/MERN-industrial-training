f1e3b3bbfed864a2f4ab13ca9c3edc1da5b0f572
Sukhmeet-0 <xsukhmeet@gmail.com>
2024-04-19 11:20:00 +0530

    adding react lec 8

---
+++ REACTJS/tut2/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button variant='primary'>Submit</Button>
+      <Button variant='outline-primary'>Submit</Button>
+    </>
+  );
+}
+
+export default App;
